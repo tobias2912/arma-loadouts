fb250cc6a3eac99f236a35572937d1abce8be692
tobias2912 <tseilertsen@gmail.com>
2020-09-13 23:36:21 +0200

    working firebaseconf

---
+++ hjemmeside/build/precache-manifest.cd5bf4aebca53456c7b7c020ec9391b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "575dfbe5a4b75ccf195fd06b4ca18a63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "570e78bcd6f534e34827",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a00e5b1b649ba45d06fa",
+    "url": "/static/js/2.0573a815.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0573a815.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "570e78bcd6f534e34827",
+    "url": "/static/js/main.c57e2b0c.chunk.js"
+  },
+  {
+    "revision": "7b96c9252b19eaecf32b",
+    "url": "/static/js/runtime-main.b10771a8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
