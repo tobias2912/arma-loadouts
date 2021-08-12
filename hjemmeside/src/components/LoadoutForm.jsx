16748fea5fe869b63fd60396917204d68f05a0e9
Tobias <tseilertsen@gmail.com>
2021-08-12 21:19:04 +0200

    add crop
    probably doesnt work

--- hjemmeside/src/components/LoadoutForm.jsx
+++ hjemmeside/src/components/LoadoutForm.jsx
@@ -13,13 +13,15 @@ import {
   Snackbar,
   TextField,
 } from "@material-ui/core";
-import React, { useContext, useState } from "react";
+import ReactCrop from 'react-image-crop';
+import React, { useCallback, useContext, useEffect, useRef, useState } from "react";
 import { postImage, postLoadout } from "../service/realtimeAPI";
 import { useStyles } from "../styles";
 import { UserContext } from "../UserProvider";
 import SendIcon from "@material-ui/icons/Send";
 import { useHistory } from "react-router-dom";
 import Alert from "@material-ui/lab/Alert";
+import "react-image-crop/dist/ReactCrop.css";
 
 export default function LoadoutForm() {
   const classes = useStyles();
@@ -28,9 +30,15 @@ export default function LoadoutForm() {
   const [classValue, setClassValue] = useState("Assault");
   const [openError, setOpen] = useState(false);
   const [errorMsg, setErrorMsg] = useState("");
-  const [selectedFile, setSelectedFile] = useState(null);
-  const [imageAsFile, setImageAsFile] = useState(null)
-  const [imageAsUrl, setImageAsUrl] = useState('')
+
+  //crop states
+
+  const [upImg, setUpImg] = useState();
+  const imgRef = useRef(null);
+  const previewCanvasRef = useRef(null);
+  const [crop, setCrop] = useState({ unit: '%', width: 30, aspect: 16 / 9 });
+  const [completedCrop, setCompletedCrop] = useState(null);
+  const [croppedFile, setCroppedFile] = useState(null);
   /**
    * gather fields from form, validate and send to server
    */
@@ -56,8 +64,6 @@ export default function LoadoutForm() {
     if (e.target.ghillie.checked) {
       attributes.push("Ghillie suit");
     }
-
-
     let loadout = {
       name: e.target.name.value,
       items: e.target.items.value,
@@ -68,13 +74,23 @@ export default function LoadoutForm() {
     if (!isValidInput(loadout)) {
       //error
     } else {
-      postImage(loadout.name, imageAsFile)
+      postCroppedImage(loadout.name);
+      console.log("original:", upImg);
       postLoadout(loadout).then(() => {
         history.push("/");
       });
     }
   };
 
+  const postCroppedImage = (name) => {
+    const canvas = previewCanvasRef.current;
+    console.log(canvas);
+    canvas.toBlob(function (blob) {
+      console.log(blob);
+      postImage((name), blob)
+    });;
+
+  }
 
   const isValidInput = (loadout) => {
     if (loadout.items.length < 20) {
@@ -91,16 +107,105 @@ export default function LoadoutForm() {
     return true;
   };
 
+  const onLoad = useCallback((img) => {
+    imgRef.current = img;
+  }, []);
+
+  function generateDownload(canvas, crop) {
+    if (!crop || !canvas) {
+      return;
+    }
+
+    canvas.toBlob(
+      (blob) => {
+        const previewUrl = window.URL.createObjectURL(blob);
+
+        const anchor = document.createElement('a');
+        anchor.download = 'cropPreview.png';
+        anchor.href = URL.createObjectURL(blob);
+        anchor.click();
+
+        window.URL.revokeObjectURL(previewUrl);
+      },
+      'image/png',
+      1
+    );
+  }
+
+  useEffect(() => {
+    if (!completedCrop || !previewCanvasRef.current || !imgRef.current) {
+      return;
+    }
+
+    const image = imgRef.current;
+    const canvas = previewCanvasRef.current;
+    const crop = completedCrop;
+
+    const scaleX = image.naturalWidth / image.width;
+    const scaleY = image.naturalHeight / image.height;
+    const ctx = canvas.getContext('2d');
+    const pixelRatio = window.devicePixelRatio;
+
+    canvas.width = crop.width * pixelRatio;
+    canvas.height = crop.height * pixelRatio;
+
+    ctx.setTransform(pixelRatio, 0, 0, pixelRatio, 0, 0);
+    ctx.imageSmoothingQuality = 'high';
+
+    ctx.drawImage(
+      image,
+      crop.x * scaleX,
+      crop.y * scaleY,
+      crop.width * scaleX,
+      crop.height * scaleY,
+      0,
+      0,
+      crop.width,
+      crop.height
+    );
+  }, [completedCrop]);
+
+  function blobCreationFromURL(inputURI) {
+  
+    var binaryVal;
+
+    // mime extension extraction
+    var inputMIME = inputURI.split(',')[0].split(':')[1].split(';')[0];
+
+    // Extract remaining part of URL and convert it to binary value
+    if (inputURI.split(',')[0].indexOf('base64') >= 0)
+        binaryVal = atob(inputURI.split(',')[1]);
+
+    // Decoding of base64 encoded string
+    else
+        binaryVal = unescape(inputURI.split(',')[1]);
+
+    // Computation of new string in which hexadecimal
+    // escape sequences are replaced by the character 
+    // it represents
+
+    // Store the bytes of the string to a typed array
+    var blobArray = [];
+    for (var index = 0; index < binaryVal.length; index++) {
+        blobArray.push(binaryVal.charCodeAt(index));
+    }
+
+    return new Blob([blobArray], {
+        type: inputMIME
+    });
+}
+
+
   const handleClassChange = () => { };
 
-  const onFileChange = event => {
-    // Update the state 
-    setSelectedFile(event.target.files[0]);
+  const onSelectFile = (e) => {
+    if (e.target.files && e.target.files.length > 0) {
+      const reader = new FileReader();
+      reader.addEventListener('load', () => setUpImg(reader.result));
+
+      reader.readAsDataURL(e.target.files[0]);
+    }
   };
-  const handleImageAsFile = (e) => {
-    const image = e.target.files[0]
-    setImageAsFile(imageFile => (image))
-  }
   return (
     <Grid xs={12} item>
       <Snackbar
@@ -264,14 +369,33 @@ export default function LoadoutForm() {
           className={classes.button}
         >
           Click to upload Image
-          <input type="file" hidden onChange={handleImageAsFile} />
+          <input type="file" hidden onChange={onSelectFile} />
         </Button>
-
+        {true ?
+          <ReactCrop
+            src={upImg}
+            onImageLoaded={onLoad}
+            crop={crop}
+            onChange={(c) => setCrop(c)}
+            onComplete={(c) => setCompletedCrop(c)}
+          />
+          :
+          <p>waiting</p>
+        }
+        <canvas
+          ref={previewCanvasRef}
+          // Rounding is important so the canvas width and height matches/is a multiple for sharpness.
+          style={{
+            width: Math.round(completedCrop?.width ?? 0),
+            height: Math.round(completedCrop?.height ?? 0)
+          }}
+        />
         <Button className={classes.button} type="submit" variant="contained" color="primary">
           send
           <SendIcon></SendIcon>
         </Button>
       </form>
+
     </Grid >
   );
 }
